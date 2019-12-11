a904af21729a132f456cfeaec5721f0ecb0df603
frostace <frostace0723@gmail.com>
2019-12-10 20:33:51 -0500

    basic structure done

---
+++ particle.js
@@ -0,0 +1,60 @@
+function Particle() {
+  this.pos = createVector(random(width), random(height));
+  // this.vel = p5.Vector.random2D();
+  this.vel = createVector(0, 0);
+  this.acc = createVector(0, 0);
+  this.maxspeed = 2;
+
+  this.prevPos = this.pos.copy();
+
+  this.update = function() {
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxspeed);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+
+  this.updatePrev = function() {
+    this.prevPos = this.pos;
+  }
+
+  this.applyForce = function(force) {
+    this.acc.add(force);
+  }
+
+  this.show = function() {
+    stroke(0, 5);
+    strokeWeight(1);
+    // point(this.pos.x, this.pos.y);
+    line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+    this.updatePrev();
+  }
+
+  this.edges = function() {
+    if (this.pos.x > width) {
+      this.pos.x = 0;
+      this.updatePrev();
+    }
+    if (this.pos.x < 0) {
+      this.pos.x = width;
+      this.updatePrev();
+    }
+    if (this.pos.y > height) {
+      this.pos.y = 0;
+      this.updatePrev();
+    }
+    if (this.pos.y < 0) {
+      this.pos.y = height;
+      this.updatePrev();
+    }
+  }
+
+  this.follow = function(vectors) {
+    var x = floor(this.pos.x / scl);
+    var y = floor(this.pos.y / scl);
+    var index = x + y * cols;
+    var force = vectors[index];
+    this.applyForce(force);
+
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,24 +1,65 @@
-var inc = 0.01;
-var scale = 20;
+var inc = 0.1;
+var scl = 10;
 var cols, rows;
 
+var zoff = 0;   // define the 3rd dimension as time
+
+// var fr;
+var particles = [];
+var flowfield;
+var particleNum = 1000;
+var fieldUpdateSpeed = 0.0004;
+
 function setup() {
-  createCanvas(200, 200);
-  cols = floor(width / scale);
-  rows = floor(height / scale);
+  createCanvas(1200, 800);
+  cols = floor(width / scl);
+  rows = floor(height / scl);
+  // fr = createP('');
+
+  flowfield = new Array(cols * rows);
+
+  for (var i = 0; i < particleNum; i++) {
+    particles[i] = new Particle();
+  }
+
 }
 
 function draw() {
+  // background(255);
   var yoff = 0;
   for (var y = 0; y < rows; y++) {
     var xoff = 0;
-    for (var y = 0; y < rows; y++) {
-      var index = (x + y * width) * 4;
-      var r = noise(xoff, yoff) * 255;
+    for (var x = 0; x < cols; x++) {
+      var index = x + y * cols;
+      var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;   // use perlin noise to generate a random angle
+      var v = p5.Vector.fromAngle(angle);             // generate vector with the random angle
+      v.setMag(1);
+      flowfield[index] = v;
       xoff += inc;
-      fill(random(255));
-      rect(x * scale, y * scale, scale, scale);
+      stroke(0, 40);
+      // push();
+      // translate(x * scl, y * scl);
+      // rotate(v.heading());
+      // strokeWeight(1);
+      // line(0, 0, scl, 0);
+
+      pop();
+      // fill(r);
+      // rect(x * scl, y * scl, scl, scl);
     }
     yoff += inc;
+    zoff += fieldUpdateSpeed;
   }
+
+  for (var i = 0; i < particleNum; i++) {
+    particles[i].follow(flowfield);
+    particles[i].update();
+    particles[i].show();
+    particles[i].edges();
+  }
+
+  // particles[0].update();
+  // particles[0].show();
+
+  // fr.html(floor(frameRate()));
 }
